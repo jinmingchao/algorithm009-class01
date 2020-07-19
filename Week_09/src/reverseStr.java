a184545f4751f57738a89bc2503dd4f81b04e451
waikato_mj120 <forgive123>
2020-07-20 00:32:23 +1200

    week 9 assignments

---
+++ Week_09/src/reverseStr.java
@@ -0,0 +1,19 @@
+/**
+ * @Author MingChao Jin
+ * @Date
+ * @Description: TODO
+ */
+public class reverseStr {
+    public String reverseStr(String s, int k) {
+        char[] a = s.toCharArray();
+        for (int start = 0; start < a.length; start += 2 * k) {
+            int i = start, j = Math.min(start + k - 1, a.length - 1);
+            while (i < j) {
+                char tmp = a[i];
+                a[i++] = a[j];
+                a[j--] = tmp;
+            }
+        }
+        return new String(a);
+    }
+}
